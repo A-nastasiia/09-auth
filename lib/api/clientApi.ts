8fafdf9713d1658a5a2ef559f934df4a62f20bfb
Anastasiia <anastasiianikolaienko2004@gmail.com>
2025-07-13 01:15:56 +0200

    0.3

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,5 +1,10 @@
 import { api } from "@/app/api/api";
 import { LogInUser, User } from "@/types/user";
+import { nextServer } from "./api";
+
+type CheckSessionResponse = {
+	success: boolean;
+};
 
 export async function register(data: User) {
   const response = await api.post<User>("/auth/register", data);
@@ -10,3 +15,13 @@ export async function login(data: User) {
   const response = await api.post<LogInUser>("/auth/login", data);
   return response.data;
 }
+
+export const checkSession = async () => {
+	const res = await nextServer.get<CheckSessionResponse>('/auth/session');
+	return res.data.success;
+};
+
+export const getMe = async () => {
+	const { data } = await nextServer.get<User>('/users/me');
+	return data;
+};
